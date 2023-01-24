4bdbc1bbd8590b2927b1c47a0ace0bc4f5cedab9
MaxAveryanov <123403824+MaxAveryanov@users.noreply.github.com>
2023-01-24 16:15:29 +0300

    Update 10_task_array.js

--- Module4/practice/10_task_array.js
+++ Module4/practice/10_task_array.js
@@ -4,3 +4,15 @@
 let names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
 
 // TODO: your code here
+let names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
+let result = '';
+let maxLenght = 0;
+for (let name of names) {
+    if (name.length > maxLenght) {
+        maxLenght = name.length;
+        result = name;
+    }
+
+
+}
+console.log(result);
