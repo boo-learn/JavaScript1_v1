46bd5dcb88deabc35fe0e6c2651c1b8d86e28b2e
Semiadmin <oleg.v.kostikov@gmail.com>
2023-01-24 23:34:00 +0300

    Update [hw]02_task_array.js

--- Module4/home_work/[hw]02_task_array.js
+++ Module4/home_work/[hw]02_task_array.js
@@ -2,3 +2,11 @@
 // Вывести на экран сумму всех положительных элементов кратных двум.
 
 // TODO: your code here
+let numbers = [10, 7, -11, 6, -4, 0];
+let sum = 0; 
+for (let number of numbers) {
+    if (number > 0 && number % 2 == 0) {
+        sum += number;   
+    }
+}
+console.log(sum);
