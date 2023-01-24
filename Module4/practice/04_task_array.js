6a48f47bc5b2b495f2b8715b08960b173f57dd04
MaxAveryanov <123403824+MaxAveryanov@users.noreply.github.com>
2023-01-24 15:11:40 +0300

    Update 04_task_array.js

--- Module4/practice/04_task_array.js
+++ Module4/practice/04_task_array.js
@@ -2,3 +2,11 @@
 // Вывести на экран сумму всех положительных элементов.
 
 // TODO: your code here
+let fruits = [2, -2, 4, 5];
+let sum = 0;
+for (let iterator of fruits) {
+    if (iterator > 0) {
+        sum += iterator;
+    }
+}
+console.log(sum);
