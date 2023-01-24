9739d977b5c739b47065b8a30ad1025268e14cd7
Semiadmin <oleg.v.kostikov@gmail.com>
2023-01-24 16:16:36 +0300

    Update 06_task_array.js

--- Module4/practice/06_task_array.js
+++ Module4/practice/06_task_array.js
@@ -1,7 +1,18 @@
 // Даны два целых числа.
 // Вывести список всех чисел кратных трем в диапазоне между заданными числами.
 
-let first_number = int(input())     // Первое число
-let second_number = int(input())    // Второе число
+//let first_number = int(input())     // Первое число
+//let second_number = int(input())    // Второе число
 
 // TODO: your code here
+let first_number = Number(prompt())     // Первое число
+let second_number = Number(prompt())    // Второе число
+if (first_number > second_number)
+{
+[first_number,second_number] = [second_number,first_number];
+}
+for (let i = first_number; i <= second_number; i++)
+{if (i % 3 == 0) {
+console.log(i);
+}
+}
