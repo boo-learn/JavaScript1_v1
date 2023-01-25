4d31a05377bb648b844a4a3a83e1f43d986c223a
MaxAveryanov <123403824+MaxAveryanov@users.noreply.github.com>
2023-01-25 14:51:25 +0300

    Update 12_task_func.js

--- Module5/practice/funcs_part2/12_task_func.js
+++ Module5/practice/funcs_part2/12_task_func.js
@@ -1,1 +1,8 @@
 // Перепишите функция average на стрелочную функцию
+let average = (...args) => {
+    sum = 0;
+    for (const number of args) {
+        sum += number
+    }
+    return sum/args.length
+}
