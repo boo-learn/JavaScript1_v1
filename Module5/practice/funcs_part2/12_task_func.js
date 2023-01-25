8a76771b09fbd93998e308d4cb5d413023fe9613
Semiadmin <oleg.v.kostikov@gmail.com>
2023-01-25 14:47:27 +0300

    Update 12_task_func.js

--- Module5/practice/funcs_part2/12_task_func.js
+++ Module5/practice/funcs_part2/12_task_func.js
@@ -1,1 +1,10 @@
 // Перепишите функция average на стрелочную функцию
+let average = (...args) => {
+let sum = 0;
+for (let arg of args) sum += arg;
+return sum / args.length
+}
+
+console.log(average(3, 4, 8))
+console.log(average(1, 4, 5, -3, 8, 4))
+console.log(average(-10, 12, 6.3, -5.5, 7, 0.2))
