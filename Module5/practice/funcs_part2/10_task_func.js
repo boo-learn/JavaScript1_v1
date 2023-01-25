4b1f4c0dc57167d550f89e1368d90749d1c6de05
Semiadmin <oleg.v.kostikov@gmail.com>
2023-01-25 14:38:40 +0300

    Update 10_task_func.js

--- Module5/practice/funcs_part2/10_task_func.js
+++ Module5/practice/funcs_part2/10_task_func.js
@@ -2,7 +2,9 @@
 // Функция должна вызываться с любым количеством аргументов
 
 function average(...args) {
-    // TODO: your code here
+let sum = 0;
+for (let arg of args) sum += arg;
+return sum / args.length
 }
 
 
