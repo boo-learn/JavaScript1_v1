449971137a4bcdf91c23b0a0aade432570cce5fd
MaxAveryanov <123403824+MaxAveryanov@users.noreply.github.com>
2023-01-25 12:04:40 +0300

    Update 03_even.js

--- Module5/practice/funcs_part0/03_even.js
+++ Module5/practice/funcs_part0/03_even.js
@@ -8,3 +8,16 @@ if (even(n))
     console.log("Число четное");
 else
     console.log("Число не четное");
+
+    
+function even(number) {
+if (number % 2 == 0) 
+    return true
+return false
+}
+
+let n = 6;
+if (even(n) == true)
+    console.log("Число четное");
+else
+    console.log("Число не четное");
